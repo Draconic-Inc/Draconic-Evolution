b6a8042a88f4b6d54c0a8de9e77fe6488ed4d09a
brandon3055 <brandon4261@gmail.com>
2014-06-23 16:31:35 +1000

    Version

--- src/main/java/com/brandon3055/draconicevolution/common/lib/References.java
+++ src/main/java/com/brandon3055/draconicevolution/common/lib/References.java
@@ -6,5 +6,5 @@ public class References {
 	public static final String VERSION = "0.9.1-Beta";
 	public static final String CLIENTPROXYLOCATION = "com.brandon3055.draconicevolution.common.core.proxy.ClientProxy";
 	public static final String COMMONPROXYLOCATION = "com.brandon3055.draconicevolution.common.core.proxy.CommonProxy";
-	public static final String RESOURCESPREFIX = MODID + ":";
+	public static final String RESOURCESPREFIX = MODID.toLowerCase() + ":";
 }
