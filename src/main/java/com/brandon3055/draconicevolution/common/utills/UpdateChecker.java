18c347be16a144ff81a39b48095a630a73193260
brandon3055 <brandon4261@gmail.com>
2015-02-24 05:27:18 +1100

    updated version

--- src/main/java/com/brandon3055/draconicevolution/common/utills/UpdateChecker.java
+++ src/main/java/com/brandon3055/draconicevolution/common/utills/UpdateChecker.java
@@ -1,5 +1,11 @@
 package com.brandon3055.draconicevolution.common.utills;
 
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 /**
  * Created by Brandon on 24/02/2015.
  */
@@ -26,15 +32,61 @@ public class UpdateChecker
 		@Override
 		public void run() {
 			LogHelper.info("Thread Run");
+
+			JsonReader reader;
 			try
 			{
-				LogHelper.info("Thread going to sleep");
-				Thread.sleep(4000L);
-				LogHelper.info("Thread resuming");
+
+				URL versionURL = new URL("https://raw.githubusercontent.com/brandon3055/Draconic-Evolution/master/VERSION.json");
+
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((versionURL).openStream()));
+
+				String cl;
+
+				while ((cl = bufferedReader.readLine()) != null) {
+					LogHelper.info(cl);
+				}
+
+
+//				File versionFile = new File("");
+//
+//				InputStream is = versionURL.openStream();
+//				OutputStream os = new FileOutputStream(versionFile);
+//				ByteStreams.copy(is, os);
+//
+//				reader = new JsonReader(new FileReader(versionFile));
+//
+//				reader.beginObject();
+//				LogHelper.info(reader.nextString());
+//
+//				while (reader.hasNext())
+//				{
+//					String name = reader.nextName();
+//
+//					if (name.equals("Version"))
+//					{
+//						LogHelper.info(reader.nextString());
+//					}
+//					else if (name.equals("Snapshot"))
+//					{
+//						LogHelper.info(reader.nextInt());
+//					}
+//					else if (name.equals("MCVersion"))
+//					{
+//						LogHelper.info(reader.nextString());
+//					}
+//					else if (name.equals("ReleaseNote"))
+//					{
+//						LogHelper.info(reader.nextString());
+//					}
+//				}
+//
+//				reader.endObject();
+//				reader.close();
+
 			}
-			catch (InterruptedException e)
+			catch (Exception e)
 			{
-				checkFaild = true;
 				e.printStackTrace();
 			}
 		}
