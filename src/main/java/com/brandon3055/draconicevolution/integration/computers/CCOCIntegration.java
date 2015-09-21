aa8efe0c007fdd8d8de2db732b832784a7be2fc6
brandon3055 <brandon4261@gmail.com>
2015-09-22 02:48:46 +1000

    CC&OC Integration Update

--- src/main/java/com/brandon3055/draconicevolution/integration/computers/CCOCIntegration.java
+++ src/main/java/com/brandon3055/draconicevolution/integration/computers/CCOCIntegration.java
@@ -1,14 +1,12 @@
 package com.brandon3055.draconicevolution.integration.computers;
 
-import com.brandon3055.draconicevolution.integration.computers.cc.DEPeripheralProvider;
+import com.brandon3055.draconicevolution.integration.computers.cc.CCAdapter;
 import com.brandon3055.draconicevolution.integration.computers.oc.DEManagedPeripheral;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Optional;
 import dan200.computercraft.api.ComputerCraftAPI;
-import dan200.computercraft.api.lua.ILuaContext;
-import dan200.computercraft.api.lua.LuaException;
-import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
+import dan200.computercraft.api.peripheral.IPeripheralProvider;
 import li.cil.oc.api.Driver;
 import li.cil.oc.api.network.ManagedEnvironment;
 import li.cil.oc.api.prefab.DriverTileEntity;
@@ -44,43 +42,19 @@ public class CCOCIntegration {
 
 		@Override
 		public ManagedEnvironment createEnvironment(World world, int x, int y, int z) {
-			TileEntity tile = world.getTileEntity(x, y, z);
-			if (tile instanceof IDEPeripheral) return new DEManagedPeripheral((IDEPeripheral)tile);
-			else return null;
+			return new DEManagedPeripheral((IDEPeripheral)world.getTileEntity(x, y, z));
 		}
 	}
 
-	public static class CCAdapter implements IPeripheral {
-		private IDEPeripheral peripheral;
-
-		public CCAdapter(IDEPeripheral peripheral){
-			this.peripheral = peripheral;
-		}
+	public static class DEPeripheralProvider implements IPeripheralProvider {
 
 		@Override
-		public String getType() {
-			return peripheral.getName();
-		}
-
-		@Override
-		public String[] getMethodNames() {
-			return peripheral.getMethodNames();
-		}
-
-		@Override
-		public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException {
-			return peripheral.callMethod(peripheral.getMethodNames()[method], arguments);
-		}
-
-		@Override
-		public void attach(IComputerAccess iComputerAccess) {}
-
-		@Override
-		public void detach(IComputerAccess iComputerAccess) {}
-
-		@Override
-		public boolean equals(IPeripheral iPeripheral) {
-			return false;
+		public IPeripheral getPeripheral(World world, int x, int y, int z, int i3) {
+			TileEntity tile = world.getTileEntity(x, y, z);
+			if (tile instanceof IDEPeripheral) {
+				return new CCAdapter((IDEPeripheral)tile);
+			}
+			else return null;
 		}
 	}
 
--- src/main/java/com/brandon3055/draconicevolution/integration/computers/oc/DEManagedPeripheral.java
+++ src/main/java/com/brandon3055/draconicevolution/integration/computers/oc/DEManagedPeripheral.java
@@ -4,16 +4,13 @@ import com.brandon3055.draconicevolution.integration.computers.IDEPeripheral;
 import li.cil.oc.api.driver.NamedBlock;
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Context;
-import li.cil.oc.api.network.ManagedEnvironment;
 import li.cil.oc.api.network.ManagedPeripheral;
-import li.cil.oc.api.network.Message;
-import li.cil.oc.api.network.Node;
-import net.minecraft.nbt.NBTTagCompound;
+import li.cil.oc.api.prefab.ManagedEnvironment;
 
 /**
  * Created by brandon3055 on 22/9/2015.
  */
-public class DEManagedPeripheral implements ManagedPeripheral, ManagedEnvironment, NamedBlock{
+public class DEManagedPeripheral extends ManagedEnvironment implements ManagedPeripheral, NamedBlock{
 	private IDEPeripheral peripheral;
 
 	public DEManagedPeripheral(IDEPeripheral peripheral){
@@ -39,14 +36,4 @@ public class DEManagedPeripheral implements ManagedPeripheral, ManagedEnvironmen
 	public int priority() {
 		return 1;
 	}
-
-	//Unused Methods
-	@Override public boolean canUpdate() { return false; }
-	@Override public void update() {}
-	@Override public Node node() {return null;}
-	@Override public void onConnect(Node node) {}
-	@Override public void onDisconnect(Node node) {}
-	@Override public void onMessage(Message message) {}
-	@Override public void load(NBTTagCompound nbtTagCompound) {}
-	@Override public void save(NBTTagCompound nbtTagCompound) {}
 }
