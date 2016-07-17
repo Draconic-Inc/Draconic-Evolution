2c02596355a6065828340651c17caada08fa48f5
brandon <brandon4261@gmail.com>
2016-07-18 00:43:20 +1000

    Updated some localization methods.

--- src/main/java/com/brandon3055/draconicevolution/client/gui/GuiEnergyCore.java
+++ src/main/java/com/brandon3055/draconicevolution/client/gui/GuiEnergyCore.java
@@ -9,9 +9,9 @@ import com.brandon3055.brandonscore.utils.Utils;
 import com.brandon3055.draconicevolution.blocks.tileentity.TileEnergyStorageCore;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.translation.I18n;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -43,9 +43,9 @@ public class GuiEnergyCore extends GuiContainer {
         super.initGui();
         buttonList.clear();
         buttonList.add(activate = new GuiButtonAHeight(0, guiLeft + 9, guiTop + 99, 162, 12, "Activate-L"));
-        buttonList.add(tierUp = new GuiButtonAHeight(1, guiLeft + 91, guiTop + 86, 80, 12, I18n.translateToLocal("button.de.tierUp.txt")));
-        buttonList.add(tierDown = new GuiButtonAHeight(2, guiLeft + 9, guiTop + 86, 80, 12, I18n.translateToLocal("button.de.tierDown.txt")));
-        buttonList.add(toggleGuide = new GuiButtonAHeight(3, guiLeft + 9, guiTop + 73, 162, 12, I18n.translateToLocal("button.de.buildGuide.txt")));
+        buttonList.add(tierUp = new GuiButtonAHeight(1, guiLeft + 91, guiTop + 86, 80, 12, I18n.format("button.de.tierUp.txt")));
+        buttonList.add(tierDown = new GuiButtonAHeight(2, guiLeft + 9, guiTop + 86, 80, 12, I18n.format("button.de.tierDown.txt")));
+        buttonList.add(toggleGuide = new GuiButtonAHeight(3, guiLeft + 9, guiTop + 73, 162, 12, I18n.format("button.de.buildGuide.txt")));
         buttonList.add(creativeBuild = new GuiButtonAHeight(4, guiLeft + 9, guiTop + ySize, 162, 12, "Creative Build"));
         updateButtonStates();
     }
@@ -54,30 +54,30 @@ public class GuiEnergyCore extends GuiContainer {
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GuiHelper.drawGuiBaseBackground(this, guiLeft, guiTop, xSize, ySize);
         GuiHelper.drawPlayerSlots(this, guiLeft + (xSize / 2), guiTop + 115, true);
-        drawCenteredString(fontRendererObj, String.format(I18n.translateToLocal("gui.de.energyStorageCore.name"), tile.tier), guiLeft + (xSize / 2), guiTop + 5, InfoHelper.GUI_TITLE);
+        drawCenteredString(fontRendererObj, String.format(I18n.format("gui.de.energyStorageCore.name"), tile.tier), guiLeft + (xSize / 2), guiTop + 5, InfoHelper.GUI_TITLE);
 
         if (tile.active.value) {
 
-            GuiHelper.drawCenteredString(fontRendererObj, I18n.translateToLocal("gui.de.capacity.txt"), guiLeft + xSize / 2, guiTop + 16, 0xFFAA00, true);
-            String capText = tile.tier.value == 8 ? I18n.translateToLocal("gui.de.almostInfinite.txt") : Utils.formatNumber(tile.getExtendedCapacity());
+            GuiHelper.drawCenteredString(fontRendererObj, I18n.format("gui.de.capacity.txt"), guiLeft + xSize / 2, guiTop + 16, 0xFFAA00, true);
+            String capText = tile.tier.value == 8 ? I18n.format("gui.de.almostInfinite.txt") : Utils.formatNumber(tile.getExtendedCapacity());
             GuiHelper.drawCenteredString(fontRendererObj, capText, guiLeft + xSize / 2, guiTop + 27, 0x555555, false);
 
-            GuiHelper.drawCenteredString(fontRendererObj, I18n.translateToLocal("info.de.charge.txt"), guiLeft + xSize / 2, guiTop + 38, 0xFFAA00, true);
+            GuiHelper.drawCenteredString(fontRendererObj, I18n.format("info.de.charge.txt"), guiLeft + xSize / 2, guiTop + 38, 0xFFAA00, true);
             GuiHelper.drawCenteredString(fontRendererObj, Utils.formatNumber(tile.getExtendedStorage()) + "RF", guiLeft + xSize / 2, guiTop + 49, 0x555555, false);
 
             int coreColour = tile.transferRate.value > 0 ? 0x00FF00 : tile.transferRate.value < 0 ? 0xFF0000 : 0x222222;
             String transfer = (tile.transferRate.value > 0 ? "+" : "") + tile.transferRate.value + " RF/t";
-            GuiHelper.drawCenteredString(fontRendererObj, I18n.translateToLocal("gui.de.transfer.txt"), guiLeft + xSize / 2, guiTop + 59, 0xFFAA00, true);
+            GuiHelper.drawCenteredString(fontRendererObj, I18n.format("gui.de.transfer.txt"), guiLeft + xSize / 2, guiTop + 59, 0xFFAA00, true);
             GuiHelper.drawCenteredString(fontRendererObj, transfer, guiLeft + xSize / 2, guiTop + 70, coreColour, tile.transferRate.value > 0);
 
         } else {
             int stabColour = tile.stabilizersOK.value ? 0x00FF00 : 0xFF0000;
-            String stabText = I18n.translateToLocal("gui.de.stabilizers.txt") + ": " + (tile.stabilizersOK.value ? I18n.translateToLocal("gui.de.valid.txt") : I18n.translateToLocal("gui.de.invalid.txt"));
+            String stabText = I18n.format("gui.de.stabilizers.txt") + ": " + (tile.stabilizersOK.value ? I18n.format("gui.de.valid.txt") : I18n.format("gui.de.invalid.txt"));
             GuiHelper.drawCenteredString(fontRendererObj, stabText, guiLeft + xSize / 2, guiTop + 18, stabColour, tile.stabilizersOK.value);
-            GuiHelper.drawCenteredString(fontRendererObj, I18n.translateToLocal("gui.de.advancedStabilizersRequired.txt"), guiLeft + xSize / 2, guiTop + 28, 0x777777, false);
+            GuiHelper.drawCenteredString(fontRendererObj, I18n.format("gui.de.advancedStabilizersRequired.txt"), guiLeft + xSize / 2, guiTop + 28, 0x777777, false);
 
             int coreColour = tile.coreValid.value ? 0x00FF00 : 0xFF0000;
-            String coreText = I18n.translateToLocal("gui.de.core.txt") + ": " + (tile.coreValid.value ? I18n.translateToLocal("gui.de.valid.txt") : I18n.translateToLocal("gui.de.invalid.txt"));
+            String coreText = I18n.format("gui.de.core.txt") + ": " + (tile.coreValid.value ? I18n.format("gui.de.valid.txt") : I18n.format("gui.de.invalid.txt"));
             GuiHelper.drawCenteredString(fontRendererObj, coreText, guiLeft + xSize / 2, guiTop + 48, coreColour, tile.coreValid.value);
         }
     }
@@ -118,11 +118,11 @@ public class GuiEnergyCore extends GuiContainer {
 
     private void updateButtonStates() {
         if (tile.active.value) {
-            activate.displayString = I18n.translateToLocal("button.de.deactivate.txt");
+            activate.displayString = I18n.format("button.de.deactivate.txt");
         } else {
-            activate.displayString = I18n.translateToLocal("button.de.activate.txt");
+            activate.displayString = I18n.format("button.de.activate.txt");
 
-            toggleGuide.displayString = I18n.translateToLocal("button.de.buildGuide.txt") + " " + (tile.buildGuide.value ? I18n.translateToLocal("gui.de.active.txt") : I18n.translateToLocal("gui.de.inactive.txt"));
+            toggleGuide.displayString = I18n.format("button.de.buildGuide.txt") + " " + (tile.buildGuide.value ? I18n.format("gui.de.active.txt") : I18n.format("gui.de.inactive.txt"));
 
             tierUp.enabled = tile.tier.value < 8;
             tierDown.enabled = tile.tier.value > 1;
