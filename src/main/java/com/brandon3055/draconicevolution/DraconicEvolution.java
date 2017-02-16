29be386e96ac66573ed0f7b422b7cd7277d892cd
Jared <jaredlll08@gmail.com>
2017-02-17 01:34:38 +0200

    Fixed a loading order issue.

--- src/main/java/com/brandon3055/draconicevolution/DraconicEvolution.java
+++ src/main/java/com/brandon3055/draconicevolution/DraconicEvolution.java
@@ -26,7 +26,7 @@ public class DraconicEvolution {
     public static final String MOD_PREFIX = MODID.toLowerCase() + ":";
     public static final String PROXY_CLIENT = "com.brandon3055.draconicevolution.client.ClientProxy";
     public static final String PROXY_SERVER = "com.brandon3055.draconicevolution.CommonProxy";
-    public static final String DEPENDENCIES = "after:NotEnoughItems;before:ThermalExpansion;after:ThermalFoundation;required-after:brandonscore@[" + BrandonsCore.VERSION + ",);";
+    public static final String DEPENDENCIES = "after:immersiveengineering;after:NotEnoughItems;before:ThermalExpansion;after:ThermalFoundation;required-after:brandonscore@[" + BrandonsCore.VERSION + ",);";
     public static final String GUI_FACTORY = "";//TODO com.brandon3055.draconicevolution.client.gui.DEGUIFactory";
     public static final String networkChannelName = "DEvolutionNC";
     //region Misc Fields
