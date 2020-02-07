b7db060b954daf927cfd9ca9fa4ea5f2eeca80b3
brandon3055 <brandon4261@gmail.com>
2020-02-07 17:55:21 +1100

    Added Charge / Discharge ETA to energy core

--- src/main/java/com/brandon3055/draconicevolution/client/gui/GuiEnergyCore.java
+++ src/main/java/com/brandon3055/draconicevolution/client/gui/GuiEnergyCore.java
@@ -1,14 +1,19 @@
 package com.brandon3055.draconicevolution.client.gui;
 
 import codechicken.lib.math.MathHelper;
+import com.brandon3055.brandonscore.client.ResourceHelperBC;
 import com.brandon3055.brandonscore.client.gui.GuiButtonAHeight;
 import com.brandon3055.brandonscore.client.utils.GuiHelper;
 import com.brandon3055.brandonscore.inventory.ContainerBCBase;
 import com.brandon3055.brandonscore.utils.InfoHelper;
 import com.brandon3055.brandonscore.utils.Utils;
 import com.brandon3055.draconicevolution.blocks.tileentity.TileEnergyStorageCore;
+import com.brandon3055.draconicevolution.utils.LogHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextFormatting;
@@ -34,11 +39,19 @@ public class GuiEnergyCore extends GuiContainer {
     private GuiButton layerMinus;
     public static int layer = -1;
 
+    //Charge/Discharge time
+    private long ticks;
+    private long seconds;
+    private long minutes;
+    private long hours;
+    private long days;
+    private long years;
+
     public GuiEnergyCore(EntityPlayer player, TileEnergyStorageCore tile) {
-        super(new ContainerBCBase<TileEnergyStorageCore>(player, tile).addPlayerSlots(10, 116));
+        super(new ContainerBCBase<TileEnergyStorageCore>(player, tile).addPlayerSlots(10, 116 + 10));
         this.tile = tile;
         this.xSize = 180;
-        this.ySize = 200;
+        this.ySize = 210;
         this.player = player;
     }
 
@@ -46,11 +59,11 @@ public class GuiEnergyCore extends GuiContainer {
     public void initGui() {
         super.initGui();
         buttonList.clear();
-        buttonList.add(activate = new GuiButtonAHeight(0, guiLeft + 9, guiTop + 99, 162, 12, "Activate-L"));
-        buttonList.add(tierUp = new GuiButtonAHeight(1, guiLeft + 91, guiTop + 86, 80, 12, I18n.format("button.de.tierUp.txt")));
-        buttonList.add(tierDown = new GuiButtonAHeight(2, guiLeft + 9, guiTop + 86, 80, 12, I18n.format("button.de.tierDown.txt")));
-        buttonList.add(toggleGuide = new GuiButtonAHeight(3, guiLeft + 9, guiTop + 73, 162, 12, I18n.format("button.de.buildGuide.txt")));
-        buttonList.add(assembleCore = new GuiButtonAHeight(4, guiLeft + 9, guiTop + 99, 162, 12, I18n.format("button.de.assembleCore.txt")));
+        buttonList.add(activate = new GuiButtonAHeight(0, guiLeft + 9, guiTop + 99 + 10, 162, 12, "Activate-L"));
+        buttonList.add(tierUp = new GuiButtonAHeight(1, guiLeft + 91, guiTop + 86 + 10, 80, 12, I18n.format("button.de.tierUp.txt")));
+        buttonList.add(tierDown = new GuiButtonAHeight(2, guiLeft + 9, guiTop + 86 + 10, 80, 12, I18n.format("button.de.tierDown.txt")));
+        buttonList.add(toggleGuide = new GuiButtonAHeight(3, guiLeft + 9, guiTop + 73 + 10, 162, 12, I18n.format("button.de.buildGuide.txt")));
+        buttonList.add(assembleCore = new GuiButtonAHeight(4, guiLeft + 9, guiTop + 99 + 10, 162, 12, I18n.format("button.de.assembleCore.txt")));
 
         buttonList.add(layerMinus = new GuiButtonAHeight(5, guiLeft + 5, guiTop - 13, 70, 12, "Layer-"));
         buttonList.add(layerPlus = new GuiButtonAHeight(6, guiLeft + 105, guiTop - 13, 70, 12, "Layer+"));
@@ -63,7 +76,7 @@ public class GuiEnergyCore extends GuiContainer {
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GuiHelper.drawGuiBaseBackground(this, guiLeft, guiTop, xSize, ySize);
-        GuiHelper.drawPlayerSlots(this, guiLeft + (xSize / 2), guiTop + 115, true);
+        GuiHelper.drawPlayerSlots(this, guiLeft + (xSize / 2), guiTop + 115 + 10, true);
         drawCenteredString(fontRenderer, I18n.format("gui.de.energyStorageCore.name", tile.tier.toString()), guiLeft + (xSize / 2), guiTop + 5, InfoHelper.GUI_TITLE);
 
         if (tile.active.value) {
@@ -78,10 +91,28 @@ public class GuiEnergyCore extends GuiContainer {
 
             int coreColour = tile.transferRate.value > 0 ? 0x00FF00 : tile.transferRate.value < 0 ? 0xFF0000 : 0x222222;
             String transfer = (tile.transferRate.value > 0 ? "+" : tile.transferRate.value < 0 ? "-" : "") + Utils.formatNumber(Math.abs(tile.transferRate.value)) + " RF/t";
-            GuiHelper.drawCenteredString(fontRenderer, I18n.format("gui.de.transfer.txt"), guiLeft + xSize / 2, guiTop + 59, 0xFFAA00, true);
+            GuiHelper.drawCenteredString(fontRenderer, I18n.format("gui.de.transfer" + (tile.transferRate.value > 0 ? ".charge" : tile.transferRate.value < 0 ? ".discharge" : "") + ".txt"), guiLeft + xSize / 2, guiTop + 59, 0xFFAA00, true);
             GuiHelper.drawCenteredString(fontRenderer, transfer, guiLeft + xSize / 2, guiTop + 70, coreColour, tile.transferRate.value > 0);
-        }
-        else {
+
+            if (tile.transferRate.value != 0) {
+                String time = "";
+                if (years > 0) {
+                    time += formatYear(years) + ", ";
+                    time += days % 365 + " Days";
+                } else if (days > 0) {
+                    time += days % 365 + " Days, ";
+                    time += (hours % 24 < 10 ? "0" : "") + hours % 24 + ":";
+                    time += (minutes % 60 < 10 ? "0" : "") + minutes % 60 + ":";
+                    time += (seconds % 60 < 10 ? "0" : "") + seconds % 60 + "." + (ticks % 20 < 10 ? "0" : "") + ticks % 20;
+                } else {
+                    time += (hours % 24 < 10 ? "0" : "") + hours % 24 + ":";
+                    time += (minutes % 60 < 10 ? "0" : "") + minutes % 60 + ":";
+                    time += (seconds % 60 < 10 ? "0" : "") + seconds % 60 + "." + (ticks % 20 < 10 ? "0" : "") + ticks % 20;
+                }
+
+                GuiHelper.drawCenteredString(fontRenderer, time, guiLeft + xSize / 2, guiTop + 70 + 10, 0x555555, false);
+            }
+        } else {
             int stabColour = tile.stabilizersOK.value ? 0x00FF00 : 0xFF0000;
             String stabText = I18n.format("gui.de.stabilizers.txt") + ": " + (tile.stabilizersOK.value ? I18n.format("gui.de.valid.txt") : I18n.format("gui.de.invalid.txt"));
             GuiHelper.drawCenteredString(fontRenderer, stabText, guiLeft + xSize / 2, guiTop + 18, stabColour, tile.stabilizersOK.value);
@@ -101,8 +132,6 @@ public class GuiEnergyCore extends GuiContainer {
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
-
-
     }
 
     @Override
@@ -111,7 +140,7 @@ public class GuiEnergyCore extends GuiContainer {
         super.drawScreen(mouseX, mouseY, partialTicks);
 
         if (tile.active.value) {
-            GuiHelper.drawEnergyBar(this, guiLeft + 5, guiTop + 82, 170, true, tile.getExtendedStorage(), tile.getExtendedCapacity(), true, mouseX, mouseY);
+            drawEnergyBar(this, guiLeft + 5, guiTop + 82 + 10, 170, true, tile.getExtendedStorage(), tile.getExtendedCapacity(), true, mouseX, mouseY);
 
             if (GuiHelper.isInRect(guiLeft + 40, guiTop + 27, xSize - 80, 8, mouseX, mouseY)) {
                 List<String> list = new ArrayList<String>();
@@ -123,6 +152,24 @@ public class GuiEnergyCore extends GuiContainer {
                 List<String> list = new ArrayList<String>();
                 list.add(TextFormatting.GRAY + "[" + Utils.addCommas(tile.getExtendedStorage()) + " RF]");
                 drawHoveringText(list, mouseX, mouseY);
+            }
+
+            if (tile.transferRate.value != 0 && GuiHelper.isInRect(guiLeft + 40, guiTop + 70 + 10, xSize - 80, 8, mouseX, mouseY)) {
+                List<String> list = new ArrayList<String>();
+                list.add(TextFormatting.AQUA + "ETA");
+                if (years > 0) {
+                    list.add(TextFormatting.GOLD + formatYear(years));
+                }
+                if (days > 0) {
+                    list.add(TextFormatting.GOLD + "" + (days % 365) + " Days");
+                }
+
+                String time = (hours % 24 < 10 ? "0" : "") + hours % 24 + ":";
+                time += (minutes % 60 < 10 ? "0" : "") + minutes % 60 + ":";
+                time += (seconds % 60 < 10 ? "0" : "") + seconds % 60 + "." + (ticks % 20 < 10 ? "0" : "") + ticks % 20;
+                list.add(TextFormatting.GOLD + "" + time);
+                drawHoveringText(list, mouseX, mouseY);
+//                LogHelper.dev(mouseX+" " + mouseY);
             }
         }
 
@@ -137,19 +184,30 @@ public class GuiEnergyCore extends GuiContainer {
     public void updateScreen() {
         super.updateScreen();
         updateButtonStates();
+
+        if (tile.transferRate.value != 0) {
+            long space = tile.transferRate.value > 0 ? tile.getExtendedCapacity() - tile.getExtendedStorage() : tile.getExtendedStorage();
+            ticks = Math.abs(space / tile.transferRate.value);
+        }
+        else {
+            ticks = 0;
+        }
+        seconds = ticks / 20L;
+        minutes = seconds / 60L;
+        hours = minutes / 60L;
+        days = hours / 24L;
+        years = days / 365L;
     }
 
     private void updateButtonStates() {
         if (tile.active.value) {
             activate.displayString = I18n.format("button.de.deactivate.txt");
-        }
-        else {
+        } else {
             activate.displayString = I18n.format("button.de.activate.txt");
             toggleGuide.displayString = I18n.format("button.de.buildGuide.txt") + " " + (tile.buildGuide.value ? I18n.format("gui.de.active.txt") : I18n.format("gui.de.inactive.txt"));
             tierUp.enabled = tile.tier.value < 8;
             tierDown.enabled = tile.tier.value > 1;
         }
-
 
         tierUp.visible = tierDown.visible = toggleGuide.visible = !tile.active.value;
         assembleCore.visible = !tile.coreValid.value;
@@ -164,15 +222,57 @@ public class GuiEnergyCore extends GuiContainer {
         if (button.id < 5) {
             tile.sendPacketToServer(output -> {
             }, button.id);
-        }
-        else {
+        } else {
             if (button == layerPlus) {
                 layer++;
-            }
-            else {
+            } else {
                 layer--;
             }
             layer = MathHelper.clip(layer, -1, 6);
         }
+    }
+
+    public static void drawEnergyBar(Gui gui, int posX, int posY, int size, boolean horizontal, long energy, long maxEnergy, boolean toolTip, int mouseX, int mouseY) {
+        ResourceHelperBC.bindTexture("textures/gui/energy_gui.png");
+        int draw = (int) ((double) energy / (double) maxEnergy * (size - 2));
+
+        boolean inRect = GuiHelper.isInRect(posX, posY, size, 14, mouseX, mouseY);
+
+        if (horizontal) {
+            int x = posY;
+            posY = posX;
+            posX = x;
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(size + (posY * 2), 0, 0);
+            GlStateManager.rotate(90, 0, 0, 1);
+        }
+
+        GlStateManager.color(1F, 1F, 1F);
+        gui.drawTexturedModalRect(posX, posY, 0, 0, 14, size);
+        gui.drawTexturedModalRect(posX, posY + size - 1, 0, 255, 14, 1);
+        gui.drawTexturedModalRect(posX + 1, posY + size - draw - 1, 14, size - draw, 12, draw);
+
+        if (horizontal) {
+            GlStateManager.popMatrix();
+        }
+
+        if (toolTip && inRect) {
+            List<String> list = new ArrayList<String>();
+            list.add(InfoHelper.ITC() + net.minecraft.util.text.translation.I18n.translateToLocal("gui.de.energyStorage.txt"));
+            list.add(InfoHelper.HITC() + Utils.formatNumber(energy) + " / " + Utils.formatNumber(maxEnergy));
+            list.add(TextFormatting.GRAY + "[" + Utils.addCommas(energy) + " RF]");
+            GuiHelper.drawHoveringText(list, mouseX, mouseY, Minecraft.getMinecraft().fontRenderer, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+        }
+    }
+
+    public static String formatYear(long value) {
+        if (value < 1000L) return value + " Years";
+        else if (value < 1000000L) return Math.round(value / 10D) / 100D + " Thousand Years";
+        else if (value < 1000000000L) return Math.round(value / 10000D) / 100D + " Million Years";
+        else if (value < 1000000000000L) return Math.round(value / 10000000D) / 100D + " Billion Years";
+        else if (value < 1000000000000000L) return Math.round(value / 10000000000D) / 100D + " Trillion Years";
+        else if (value < 1000000000000000000L) return Math.round(value / 10000000000000D) / 100D + " Quadrillion Years";
+        else if (value <= Long.MAX_VALUE) return Math.round(value / 10000000000000000D) / 100D + " Quintillion Years";
+        else return "Something is very broken!!!!";
     }
 }
