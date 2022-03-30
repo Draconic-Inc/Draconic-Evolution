58e4e4df9a0bc39370bc253296abcde3dde3908e
brandon3055 <brandon4261@gmail.com>
2022-03-30 17:40:47 +1100

    Fixed edge case crash with energy core

--- src/main/java/com/brandon3055/draconicevolution/blocks/tileentity/TileEnergyCore.java
+++ src/main/java/com/brandon3055/draconicevolution/blocks/tileentity/TileEnergyCore.java
@@ -351,9 +351,13 @@ public class TileEnergyCore extends TileBCore implements ITickableTileEntity, IE
 
                     for (int dist = 0; dist < 16; dist++) {
                         BlockPos pos1 = worldPosition.offset(facing.getStepX() * dist, facing.getStepY() * dist, facing.getStepZ() * dist);
-                        TileEntity stabilizer = level.getBlockEntity(pos1);
-                        if (stabilizer instanceof TileEnergyCoreStabilizer && (!((TileEnergyCoreStabilizer) stabilizer).hasCoreLock.get() || ((TileEnergyCoreStabilizer) stabilizer).getCore().equals(this)) && ((TileEnergyCoreStabilizer) stabilizer).isStabilizerValid(tier.get(), this)) {
-                            stabsFound.add((TileEnergyCoreStabilizer) stabilizer);
+                        TileEntity tile = level.getBlockEntity(pos1);
+
+                        if (!(tile instanceof TileEnergyCoreStabilizer)) continue;
+                        TileEnergyCoreStabilizer stabilizer = (TileEnergyCoreStabilizer) tile;
+
+                        if (stabilizer.getCore() == this && stabilizer.isStabilizerValid(tier.get(), this)) {
+                            stabsFound.add(stabilizer);
                             break;
                         }
                     }
