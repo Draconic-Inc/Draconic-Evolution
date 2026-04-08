411af9b2bdc644117c1276f815a6cedb58e14d92
FoxMcloud5655 <support@foxmcloud.net>
2026-04-08 18:54:37 -0500

    Prevent existing worlds from breaking
    Due to changes within EnergyLinkEntity.java previously, adding a new codec breaks existing worlds.  Making it optional instead prevents the world from no longer loading if any player has an existing Energy Link without this new codec.

--- src/main/java/com/brandon3055/draconicevolution/api/modules/entities/EnergyLinkEntity.java
+++ src/main/java/com/brandon3055/draconicevolution/api/modules/entities/EnergyLinkEntity.java
@@ -42,7 +42,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
     private Optional<UUID> linkId = Optional.empty();
     //This shouldn't be saved to item, or maybe just clear on insert?
     private long linkCharge = 0;
-    private double flow = 0;
+    private Optional<Double> flow = Optional.empty();
 
     //Component data can probably still be set from entity constructor.
     private BooleanProperty enabled = new BooleanProperty("energy_link_mod.enabled", true).setFormatter(ConfigProperty.BooleanFormatter.ENABLED_DISABLED);
@@ -57,7 +57,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
             GlobalPos.CODEC.optionalFieldOf("linked_pos").forGetter(e -> e.linkedPos),
             UUIDUtil.CODEC.optionalFieldOf("link_id").forGetter(e -> e.linkId),
             Codec.LONG.fieldOf("link_charge").forGetter(e -> e.linkCharge),
-            Codec.DOUBLE.fieldOf("flow").forGetter(e -> e.flow),
+            Codec.DOUBLE.optionalFieldOf("flow").forGetter(e -> e.flow),
             BooleanProperty.CODEC.fieldOf("enabled").forGetter(e -> e.enabled)
             ).apply(builder, EnergyLinkEntity::new));
 
@@ -68,7 +68,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
             ByteBufCodecs.optional(GlobalPos.STREAM_CODEC), energyLinkEntity -> energyLinkEntity.linkedPos,
             ByteBufCodecs.optional(UUIDUtil.STREAM_CODEC), energyLinkEntity -> energyLinkEntity.linkId,
             ByteBufCodecs.VAR_LONG, energyLinkEntity -> energyLinkEntity.linkCharge,
-            ByteBufCodecs.DOUBLE, energyLinkEntity -> energyLinkEntity.flow,
+            ByteBufCodecs.optional(ByteBufCodecs.DOUBLE), energyLinkEntity -> energyLinkEntity.flow,
             BooleanProperty.STREAM_CODEC, energyLinkEntity -> energyLinkEntity.enabled,
             EnergyLinkEntity::new
     );
@@ -77,7 +77,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
         super(module);
     }
 
-    EnergyLinkEntity(Module<?> module, int gridX, int gridY, Optional<GlobalPos> linkedPos, Optional<UUID> linkId, long linkCharge, double flow, BooleanProperty enabled) {
+    EnergyLinkEntity(Module<?> module, int gridX, int gridY, Optional<GlobalPos> linkedPos, Optional<UUID> linkId, long linkCharge, Optional<Double> flow, BooleanProperty enabled) {
         super((Module<EnergyLinkData>) module, gridX, gridY);
         this.linkedPos = linkedPos;
         this.linkId = linkId;
@@ -136,7 +136,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
         if (!updateConnection(data, storage, core)) return;
 
         long inserted = core.energy.extractOP(storage.modifyEnergyStored(core.energy.extractOP(data.transferLimit(), true)), false);
-        flow = MathHelper.approachExp(flow, inserted, 1 / 20D);
+        flow = Optional.of(MathHelper.approachExp(flow.orElse(0D), inserted, 1 / 20D));
         markDirty();
     }
 
@@ -160,7 +160,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
             return false;
         }
 
-        long maintenanceCost = (long) ((data.operationEnergy() * 0.1) + (data.operationEnergy() * 0.9 * (flow / data.transferLimit())));
+        long maintenanceCost = (long) ((data.operationEnergy() * 0.1) + (data.operationEnergy() * 0.9 * (flow.orElse(0D) / data.transferLimit())));
         long extracted = core.energy.extractOP(maintenanceCost, false);
         if (extracted < maintenanceCost) {
             disconnect();
@@ -172,7 +172,7 @@ public class EnergyLinkEntity extends ModuleEntity<EnergyLinkData> {
 
     private void disconnect() {
         linkCharge = 0;
-        flow = 0;
+        flow = Optional.of(0D);
         markDirty();
     }
 
